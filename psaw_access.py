6995bf7bc3366a4c9fe2629a716f2c6350cead6a
FCRA <fcrargenziano@gmail.com>
2021-03-22 16:17:01 +0100

    correction on GME section

--- psaw_access.py
+++ psaw_access.py
@@ -3,15 +3,15 @@ import datetime as dt
 import logging as log
 
 api = PushshiftAPI()
-start_time =int(dt.datetime(2021, 1, 20).timestamp())   #(2020, 12, 20) --> (2021, 1, 20)
-end_time = int(dt.datetime(2021,  2, 1).timestamp())     #(2021, 1, 20) --> (2021,  2, 1) --> (2021,  2, 18)
+start_time =int(dt.datetime(2020, 12, 20).timestamp())   #(2020, 12, 20) --> (2021, 1, 20)
+end_time = int(dt.datetime(2021,  2, 18).timestamp())     #(2021, 1, 20) --> (2021,  2, 1) --> (2021,  2, 18)
 
 submissions = api.search_submissions(
                             after=start_time,
                             before=end_time,
                             subreddit='wallstreetbets',
                             filter=['url','author', 'title', 'subreddit'], 
-                            #limit= 500
+                            #limit= 100
                             )
 """
 def cycle_in_tf(end_year, end_month, end_day): 
@@ -26,12 +26,11 @@ mysubs = []
 with open('wsb_all.txt', 'w') as f: 
     for sub in submissions: 
         wrds = sub.title.split()
-        for i in wrds: 
-            if "GME" not in i: continue 
-            else: 
-                mysubs.append(sub)
-                log.warning(f"Sub title: {sub.title}")
-                f.write(f"{sub}\n")
+        if "GME" not in wrds: continue 
+        else: 
+            mysubs.append(sub)
+            log.warning(f"Sub title: {sub.title}")
+            f.write(f"{sub}\n")
 
 log.warning("End.")
 
