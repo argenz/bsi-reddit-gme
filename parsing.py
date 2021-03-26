8d7a9cf7231f065f38880258d8a54828818d53fb
FCRA <fcrargenziano@gmail.com>
2021-03-26 22:58:10 +0100

    plot

--- parsing.py
+++ parsing.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os 
 import datetime as dt
+import matplotlib.pyplot as plt
 
 # -- -- -- -- Parsing from CSV to pandas.DataFrame -- -- -- -- #
 df_dict = {}
@@ -9,13 +10,26 @@ for filename in os.listdir("./files"):
 
 sub_df = pd.concat([df for df in df_dict.values()]).reset_index(drop=True)
 
-#print(sub_df.head())
-#print(sub_df.tail())
-
 # convert utc to normal date
 sub_df["created_utc"] = sub_df["created_utc"].apply(lambda x: dt.datetime.fromtimestamp(int(x)).strftime("%Y-%m-%d %H:%M:%S"))
+sub_df.rename(columns={'created_utc':'created'}, inplace=True)
 
-#print(len(sub_df.index))
-#print(sub_df.loc[478])
 #print(sub_df.head())
 #print(sub_df.tail())
+
+# -- -- -- -- Plot number of Subs per day -- -- -- -- #
+
+sub_df_date_count = (pd.to_datetime(sub_df['created'])
+       .dt.floor('d')
+       .value_counts()
+       .rename_axis('created')
+       .reset_index(name='count')
+       .sort_values('created', ascending=False))
+
+#print(sub_df_date_count)
+
+plt.plot(sub_df_date_count['created'], sub_df_date_count["count"], "o-")
+plt.show()
+
+
+
