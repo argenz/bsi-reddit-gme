95a6e552f4eef695ff558b95362abb9b5b79b4f7
ThePres <mss.mrc.mm@gmail.com>
2021-04-06 18:37:51 +0200

    Piccole modifiche

--- Stock.py
+++ Stock.py
@@ -7,15 +7,18 @@ import pandas as pd
 # get info stock
 GME = yf.Ticker("GME")
 
-rawdata = GME.history(start="2020-03-20", end="2020-05-20", interval="30m", prepost= True)
+rawdata = GME.history(start="2020-03-20", end="2020-05-20", interval="1h", prepost= True)
 print(rawdata)
 print("hello!")
 
 # graph data
 plt.plot(rawdata.index, rawdata["Close"],)
-#print(rawdata.index)
-#print(rawdata.columns)
+print(rawdata.index)
+print(rawdata.columns)
+plt.show()
 
 #new column with time data
-
+rawdata.index
+time = rawdata.index
+rawdata['time'] = time
 
