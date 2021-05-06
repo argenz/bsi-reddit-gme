e37009207a7a4750c25ce5a713bd5a54efb60047
FCRA <fcrargenziano@gmail.com>
2021-05-06 10:32:40 +0200

    Lots of changes

--- pyfiles/analysis.py
+++ pyfiles/analysis.py
@@ -6,7 +6,7 @@ import numpy as np
 import regex as re
 from collections import Counter
 
-sub_df = pd.read_csv("/bsi-reddit-gme/pyfiles/sub_df.csv"")
+sub_df = pd.read_csv("/Users/FCRA/Desktop/ALL/BSI/bsi-reddit-gme/pyfiles/sub_df.csv")
 
 # -- -- -- -- Plot number of Subs per day -- -- -- -- #
 
@@ -38,28 +38,28 @@ sub_df_date_count["z_count"] = sub_df_date_count["count"]/sub_std
 stock_std = stock_data.std(axis=0).iloc[1]
 stock_data["z_open"] = stock_data["Open"]/stock_std
 
-# plt.figure()
-# plt.plot(stock_data['Date'], stock_data["z_open"], "o-", label="Stock Price")
-# plt.plot(sub_df_date_count["created"], sub_df_date_count["z_count"], "x-", label="Sub Count")
-# plt.title("Standardised Price and Submission Count plotted daily")
-# plt.xlabel('Day')
-# plt.ylabel('z')
-# plt.legend()
-# plt.show()
+plt.figure()
+plt.plot(stock_data['Date'], stock_data["z_open"], "o-", label="Stock Price")
+plt.plot(sub_df_date_count["created"], sub_df_date_count["z_count"], "x-", label="Sub Count")
+plt.title("Standardised Price and Submission Count plotted daily")
+plt.xlabel('Day')
+plt.ylabel('z')
+plt.legend()
+plt.show()
 
 # warning: only plotting open prices 
 # TODO: standardise average between high and low of each day, and plot
 
 # -- Title Analysis -- #
 
-title_df = sub_df["title"].str.lower()
-title_df = title_df.str.replace(r'[^\w\s]', '')  #replace any character that is not a word or a space with nothing
-words = " ".join(title for title in title_df)
-wd_freq_df = pd.DataFrame(Counter(words.split()), index=[1]).transpose().reset_index().rename(columns={"index": "word", 1: "frequency"}).sort_values(by=["frequency"], ascending=False)
+#title_df = sub_df["title"].str.lower()
+#title_df = title_df.str.replace(r'[^\w\s]', '')  #replace any character that is not a word or a space with nothing
+#words = " ".join(title for title in title_df)
+#wd_freq_df = pd.DataFrame(Counter(words.split()), index=[1]).transpose().reset_index().rename(columns={"index": "word", 1: "frequency"}).sort_values(by=["frequency"], ascending=False)
 
 # order per most frequent to least frequent 
 # inspect the df, remove prepositions, articles, http addresses?... 
 
 
 # WSB Lingo
-lingo_df = pd.DataFrame({"lingo": ["diamond hand", "paper hand", "stonk", "tendie", "to the moon", "jpow", "yolo"]})
+#lingo_df = pd.DataFrame({"lingo": ["diamond hand", "paper hand", "stonk", "tendie", "to the moon", "jpow", "yolo"]})
--- pyfiles/sentiment.py
+++ pyfiles/sentiment.py
@@ -2,28 +2,18 @@ import pandas as pd
 from textblob import TextBlob, Word
 import matplotlib.pyplot as plt
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-from lexicon import new_words, remove_emoji, space
+from lexicon import new_words, new_words_updated, remove_emoji, space, update_new_words
 from nltk.corpus import stopwords
 
 from analysis import stock_data
-
 # -- Sentiment Analysis -- # 
 
-sub_df = pd.read_csv("/bsi-reddit-gme/pyfiles/sub_df.csv")
+sub_df = pd.read_csv("/Users/FCRA/Desktop/ALL/BSI/bsi-reddit-gme/pyfiles/sub_df.csv")
 sent_df = sub_df[["created", "author", "title"]]
 
-# sub_df2 = pd.read_csv("preproc_titles.csv").reset_index(drop=True)
-# sent_df["ptitle"] = sub_df2["title"]
-
-# --- Preprocessing 
-sent_df["ptitle"] = sub_df["title"].str.lower().str.replace(r'[^\w\s]', '')
-sent_df["ptitle"] = sent_df["ptitle"].apply(lambda x: remove_emoji(x))
-
-#remove english stop words
-#stop = stopwords.words('english')
-#sent_df["ptitle"] = sent_df["ptitle"].apply(lambda x: " ".join(x for x in x.split() if x not in stop))
-
-#lemmatization in google colab
+# already preproc titles
+sub_df2 = pd.read_csv("/Users/FCRA/Desktop/ALL/BSI/bsi-reddit-gme/sentiment_files/preproc_titles.csv").reset_index(drop=True)
+sent_df["ptitle"] = sub_df2["title"]
 
 # --- General Sentiment of Titles with TextBlob
 
@@ -33,30 +23,41 @@ sent_df["created"] = pd.to_datetime(sent_df["created"]).dt.floor('d')
 daily_sent_df_textBlob = sent_df[["created", "polarity_textBlob"]].groupby(["created"], as_index=False).mean()
 daily_sent_df_textBlob["z_polarity_textBlob"] = daily_sent_df_textBlob["polarity_textBlob"]/daily_sent_df_textBlob["polarity_textBlob"].std(axis=0)
 
-sent_df[["ptitle", "polarity_textBlob"]].to_csv("titles_textblob.csv")
+#sent_df[["ptitle", "polarity_textBlob"]].to_csv("titles_textblob.csv")
+
 # --- Sentiment using Vader and styled lexicon
 
-# vader = SentimentIntensityAnalyzer()
-# vader.lexicon.update(new_words)
+vader = SentimentIntensityAnalyzer()
+vader.lexicon.update(new_words)
 
-# sent_df["polarity_vader"] = sent_df["ptitle"].apply(lambda x: vader.polarity_scores(x)["compound"])
-# # sent_df[sent_df["polarity_vader"]>= 0.8].to_csv("TopPolarityVader")
-# # sent_df[sent_df["polarity_vader"]<= 0.2].to_csv("BottomPolarityVader")
+sent_df["polarity_vader"] = sent_df["ptitle"].apply(lambda x: vader.polarity_scores(x)["compound"])
+# sent_df[sent_df["polarity_vader"]>= 0.8].to_csv("TopPolarityVader")
+# sent_df[sent_df["polarity_vader"]<= 0.2].to_csv("BottomPolarityVader")
 
-# daily_sent_df_vader = sent_df[["created", "polarity_vader"]].groupby(["created"], as_index=False).mean()
-# daily_sent_df_vader["z_polarity_v"] = daily_sent_df_vader["polarity_vader"]/daily_sent_df_vader["polarity_vader"].std(axis=0)
+daily_sent_df_vader = sent_df[["created", "polarity_vader"]].groupby(["created"], as_index=False).mean()
+daily_sent_df_vader["z_polarity_v"] = daily_sent_df_vader["polarity_vader"]/daily_sent_df_vader["polarity_vader"].std(axis=0)
 
+# # --- New Words
+df_most_freq = pd.read_csv("/Users/FCRA/Desktop/ALL/BSI/bsi-reddit-gme/Notebooks/frequent_words.csv")
+incoming_words = df_most_freq["word"].tolist()
+new_word_set = update_new_words(new_words, incoming_words)
+
+vader.lexicon.update(new_words_updated)
+
+sent_df["polarity_vader_up"] = sent_df["ptitle"].apply(lambda x: vader.polarity_scores(x)["compound"])
+daily_sent_df_vader_up = sent_df[["created", "polarity_vader_up"]].groupby(["created"], as_index=False).mean()
+daily_sent_df_vader_up["z_polarity_vup"] = daily_sent_df_vader_up["polarity_vader_up"]/daily_sent_df_vader_up["polarity_vader_up"].std(axis=0)
 
 # # --- Plotting
 
-# plt.figure()
-# plt.plot(daily_sent_df_vader["created"], daily_sent_df_vader["z_polarity_v"], label="z_Polarity_vader")
-# plt.plot(daily_sent_df_textBlob["created"], daily_sent_df_textBlob["z_polarity_textBlob"], label="z_Polarity_textBlob")
-# plt.plot(stock_data['Date'], stock_data["z_open"], "o-", label="z_Stock_Price")
-# plt.xlabel("Day")
-# plt.ylabel("Polarity & Stock Price")
-# plt.legend()
-# plt.show()
+plt.figure()
+plt.plot(daily_sent_df_vader["created"], daily_sent_df_vader["z_polarity_v"], label="z_Polarity_vader")
+plt.plot(daily_sent_df_vader["created"], daily_sent_df_vader_up["z_polarity_vup"], label="z_Polarity_vader_update")
+plt.plot(daily_sent_df_textBlob["created"], daily_sent_df_textBlob["z_polarity_textBlob"], label="z_Polarity_textBlob")
+plt.plot(stock_data['Date'], stock_data["z_open"], "o-", label="z_Stock_Price")
+plt.xlabel("Day")
+plt.ylabel("Polarity & Stock Price")
+plt.legend()
+plt.show()
 
-# # --- 
 
